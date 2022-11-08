b1c1174b78e44df71bf3d99ede08529d648e3239
Tetiana <shulga.tati@gmail.com>
2022-11-08 22:58:01 +0200

    Merge branch 'main' of https://github.com/TetianaZalata/Hometask-project-setup-unit-tests-integration-tests into main

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Hometask-project-setup-unit-tests-integration-tests
+
+## Requirements
+
+Clone this repository to your workstation.
+
+## Tests
+
+- Run `yarn install` to install all dependencies.
+- Run `yarn debug` to check debug command output.
+- Run `yarn test` to check test command output.
--- tests/rectangle.test.js
+++ tests/rectangle.test.js
@@ -1,11 +1,17 @@
-import { getRectanglePerimeter, getRectangleArea } from '../js/rectangle';
+import { getRectanglePerimeter, getRectangleArea, getRectangleInfo } from '../js/rectangle';
 
 
-test('adds 2 + 1 and multiply to 2 equal 6', () => {
+test('2 added to 1 then multiplied by 2, equals 6', () => {
   expect(getRectanglePerimeter(2, 1)).toBe(6);
 });
 
-test('3 multiply to 2  equal 6', () => {
+test('3 multiplied by 2, equals 6', () => {
   expect(getRectangleArea(3, 2)).toBe(6);
 });
 
+test('should generate a valid text output', () => {
+  const output = getRectangleInfo(4, 2);
+  const perimeter = getRectanglePerimeter(4, 2);
+  const area = getRectangleArea (4, 2);
+  expect(output).toBe(console.log (`The perimeter of a rectangle is ${perimeter} and the area is ${area}`));
+});
